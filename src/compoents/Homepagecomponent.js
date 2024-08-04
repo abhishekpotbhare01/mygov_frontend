15451275695641feb36eafa74631c808cf61e8d3
abhishekpotbhare01 <abhishek.potbhare01@gmail.com>
2024-08-04 15:42:40 +0530

    changes

--- src/compoents/Homepagecomponent.js
+++ src/compoents/Homepagecomponent.js
@@ -6,6 +6,8 @@ import two from "../assets/gov/img/gov_schme.png";
 
 import img1 from "../assets/gov/img/ladli-behna-yojana.png"
 
+import pmkisan from "../assets/gov/img/pm_kisan.png"
+
 import { Navbar } from "./Navbar";
 import HomePage from "./HomePage";
 
@@ -17,33 +19,6 @@ function Homepagecomponent() {
 
       <HomePage></HomePage>
       <div class="main">
-        {/**  <!-----------------------------------Slider---------------------------------->*/}
-        {/* <div id="carouselExampleIndicators" className="carousel slide carousel-custom-height col-12" data-ride="carousel"   >
-      <ol className="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div className="carousel-inner">
-        <div className="carousel-item ">
-          <img className="d-block w-100" src={one} alt="First slide" />
-        </div>
-        <div className="carousel-item active">
-          <img className="d-block w-100" src={two} alt="Second slide" />
-        </div>
-        <div className="carousel-item">
-          <img className="d-block w-100" src={"images/carousel3.jpg"} alt="Third slide" />
-        </div>
-      </div>
-      <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
-    </div> */}
 
 
         {/** <!------------------------------Cards--------------------------------> */}
@@ -80,20 +55,62 @@ function Homepagecomponent() {
                 ></div>
 
                 <p class="card-text ">
-                Ladli Behna Yojana की मध्‍य प्रदेश सरकार द्वारा 28 जनवरी 2023 को आरम्‍भ की गई थी।
-                 इसका उद्येश्‍य महिलाओं के स्वास्थ्य एवं पोषण तथा आर्थिक स्वावलम्बन को बढाना है। इस योजना के
-                 तहत मध्‍य प्रदेश राज्‍य की महिलाओं को 1000 रूपये प्रतिमाह प्रदान किये जाएंगे।
+                  Ladli Behna Yojana की मध्‍य प्रदेश सरकार द्वारा 28 जनवरी 2023 को आरम्‍भ की गई थी।
+                  इसका उद्येश्‍य महिलाओं के स्वास्थ्य एवं पोषण तथा आर्थिक स्वावलम्बन को बढाना है। इस योजना के
+                  तहत मध्‍य प्रदेश राज्‍य की महिलाओं को 1000 रूपये प्रतिमाह प्रदान किये जाएंगे।
                 </p>
 
               </div>
             </div>
 
-          
+            <div class="card ">
 
-             
-          
+              <div
+                style={{
+                  display: "flex",
+                  alignitems: "center",
+                  justifycontent: "center",
+                }}
+              >
+                <img src={pmkisan} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+
+                  alignitems: "center",
+                  justifycontent: "center",
+                }}
+              >
+
+              </div>
+
+              <div class="card-body">
+                <div
+                  style={{
+                    display: "flex",
+                    alignitems: "center",
+                    justifycontent: "center",
+                  }}
+                ></div>
+
+                <p class="card-text ">
+                प्रधानमंत्री किसान सन्मान निधी योजना ही शेतकऱ्यांना आर्थिक मदत देण्यासाठी भारत सरकारची विशेष योजना आहे.
+                 यासाठी पूर्णपणे भारत सरकारकडून निधी दिला जातो. 
+                 या योजनेअंतर्गत सर्व भू-धारक शेतकरी कुटुंबांना तीन समान हप्त्यांमध्ये वार्षिक ६००० रुपयांचं सहाय्य दिलं जातं.
+                </p>
+
+              </div>
+            </div>
+
+
+
+
+
           </div>
         </div>
+
+
 
       </div>
     </>
