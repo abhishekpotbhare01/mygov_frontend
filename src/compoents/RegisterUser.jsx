dc0ba665c0a77e6eb0bee101542b336ff29f77cf
abhishekpotbhare01 <abhishek.potbhare01@gmail.com>
2024-08-05 11:26:23 +0530

    User service changes

--- src/compoents/RegisterUser.jsx
+++ src/compoents/RegisterUser.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import UserService from '../service/UserService';
+
+
 const RegisterUser = () => {
     const [formData, setFormData] = useState({
         name: '',
@@ -17,12 +20,27 @@ const RegisterUser = () => {
             [name]: value
         });
     };
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // registration logic 
 
-        console.log('Form submitted:', formData);
+        if (formData.password !== formData.confirmPassword) {
+            setErrorMessage('Passwords do not match');
+            setSuccessMessage('');
+            return;
+        }
+
+        try {
+            const response = await UserService.AddUser(formData);
+            setSuccessMessage('Form submitted successfully');
+            setErrorMessage('');
+        } catch (error) {
+            console.error('Form submission failed:', error);
+            setErrorMessage('Form submission failed: ' + error.message);
+            setSuccessMessage('');
+        }
     };
 
     const [showPassword, setShowPassword] = useState(false);
@@ -45,7 +63,7 @@ const RegisterUser = () => {
                     />
                 </div>
                 <div className="form-group">
-                    {/* <label htmlFor="email">Email</label> */}
+
                     <input
                         type="email"
                         className="form-control"
@@ -57,7 +75,7 @@ const RegisterUser = () => {
                     />
                 </div>
                 <div className="form-group">
-                    {/* <label htmlFor="age">Age</label> */}
+
                     <input
                         type="number"
                         className="form-control"
@@ -69,7 +87,6 @@ const RegisterUser = () => {
                     />
                 </div>
                 <div className="form-group d-flex flex-row">
-                    {/* <label htmlFor="password">Password</label> */}
                     <input
                         type={showPassword ? 'text' : 'password'}
                         className="form-control col-md-10"
@@ -85,22 +102,22 @@ const RegisterUser = () => {
                     >
                         {showPassword ?
                             <svg width="35px" height="35px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                <circle cx="12" cy="12" r="3" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                <circle cx="12" cy="12" r="3" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
                             :
                             <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 2L22 22" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                <path d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M2 2L22 22" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
 
                         }
                     </div>
                 </div>
                 <div className="form-group">
-                    {/* <label htmlFor="confirmPassword">Confirm Password</label> */}
+
                     <input
                         type="password"
                         className="form-control"
@@ -111,6 +128,8 @@ const RegisterUser = () => {
                         placeholder='Re-Enter Password'
                     />
                 </div>
+                {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+                {successMessage && <div className="alert alert-success">{successMessage}</div>}
                 <div className="d-flex justify-content-between">
                     <button type="submit" className="btn btn-primary btn-block col-md-6">
                         Register
